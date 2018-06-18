fdde0edd3ea094ffc9c496c382bfb600ed164c59
Mari Queiroz <mqueiroz@MacBook-Pro-de-Mari-4.local>
2018-06-18 19:32:21 -0300

    Conta de Banco

--- POO-Aula05-ContaBanco/src/aula05/Main.java
+++ POO-Aula05-ContaBanco/src/aula05/Main.java
@@ -2,6 +2,28 @@ package aula05;
 
 public class Main {
     public static void main(String[] args) {
+        ContaBanco conta01 = new ContaBanco("Mari");
+
+        conta01.abreConta("Corrente");
+
+        conta01.status();
+
+        conta01.deposito(53.2f);
+
+        conta01.saque(10f);
+
+
+        conta01.deposito(0f);
+
+        conta01.saque(0f);
+
+        conta01.fechaConta();
+
+        conta01.saque(5f);
+        conta01.deposito(3f);
+
+
+        conta01.status();
 
     }
 
